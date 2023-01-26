d201255ea26568e5109b19ddc571fc7792104905
Gleb <glebmironov@mail.ru>
2023-01-26 18:38:56 +0300

    HW-Pagination

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,5 +1,8 @@
+import csv
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from .settings import BUS_STATION_CSV
 
 
 def index(request):
@@ -7,11 +10,17 @@ def index(request):
 
 
 def bus_stations(request):
-    current_page = 1
-    next_page_url = 'write your url'
+    CONTENT = []
+    with open(BUS_STATION_CSV, encoding='cp1251') as csvfile:
+        data = csv.DictReader(csvfile)
+        for row in data:
+            CONTENT.append(row)
+    paginator = Paginator(CONTENT, 10)
+    current_page = int(request.GET.get('page', 1))
+    next_page_url = f'?page={current_page + 1}'
+    pagi_data = paginator.get_page(current_page)
     return render(request, 'index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'},
-                         {'Name': 'другое название', 'Street': 'другая улица', 'District': 'другой район'}],
+        'bus_stations': pagi_data,
         'current_page': current_page,
         'prev_page_url': None,
         'next_page_url': next_page_url,
