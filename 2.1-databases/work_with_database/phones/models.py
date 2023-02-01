0836bcca4cd8643c59e37654ad79c5ecd9a0fa4b
Gleb <glebmironov@mail.ru>
2023-02-01 17:49:18 +0300

    HW 2.1 work_with_database Done

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,12 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=60)
+    price = models.FloatField()
+    image = models.URLField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField()
+
+    def __str__(self):
+        return f'{self.name}, {self.price}: {self.slug}'
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,27 @@
 from django.shortcuts import render, redirect
-
+from .models import Phone
 
 def index(request):
     return redirect('catalog')
 
 
 def show_catalog(request):
+    sort = request.GET.get('sort')
     template = 'catalog.html'
-    context = {}
+    if sort == 'name':
+        phones_obj = Phone.objects.order_by(sort)
+    elif sort == 'min_price':
+        phones_obj = Phone.objects.order_by('price')
+    elif sort == 'max_price':
+        phones_obj = Phone.objects.order_by('-price')
+    else:
+        phones_obj = Phone.objects.all()
+    context = {'phones': phones_obj}
     return render(request, template, context)
 
 
 def show_product(request, slug):
+    phone = Phone.objects.get(slug=slug)
     template = 'product.html'
-    context = {}
+    context = {'phone': phone}
     return render(request, template, context)
