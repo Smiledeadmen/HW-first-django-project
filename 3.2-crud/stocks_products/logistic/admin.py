caf4e1584ec0e59bcdbc4782f839bc65ee774138
Gleb <glebmironov@mail.ru>
2023-03-24 14:37:23 +0300

    HW 3.2 CRUD Done

--- 3.2-crud/stocks_products/logistic/admin.py
+++ 3.2-crud/stocks_products/logistic/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Product, Stock
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
 
-# Register your models here.
+
+@admin.register(Stock)
+class StockAdmin(admin.ModelAdmin):
+    pass
--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
-
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 
@@ -7,10 +8,12 @@ from logistic.serializers import ProductSerializer, StockSerializer
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['title',]
+    search_fields = ['description', 'title']
 
 
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    # при необходимости добавьте параметры фильтрации
+    filterset_fields = ['products']
